cb71889713213b2548fe76462234e24478005956
shlomi991 <97621005+shlomi991@users.noreply.github.com>
2023-01-02 00:07:20 +0200

    Update kube.py

--- kube.py
+++ kube.py
@@ -7,18 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Don hamelech!"
+    return "hello"
 
 @app.route("/example")
 def soya():
-    # return ("Client core shit")
     config.load_kube_config
     v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
         api_response = v1.read_namespaced_pod_log(i.metadata.name, namespace='default')
-        print(api_response)
+        return api_response
 
 
 if __name__ == '__main__':
